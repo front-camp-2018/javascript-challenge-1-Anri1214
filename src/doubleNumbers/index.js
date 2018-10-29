1190c0cbd88d6f0d5923cda8593b8d3ba4c115ee
Anri1214 <Anri1214.K@gmail.com>
2018-10-29 21:15:19 +0200

    Complete test task

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,10 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const numStr = num.toString();
+  const length = numStr.length;
+  const half = length / 2;
+  const first = numStr.substr(0, half);
+  const second = numStr.substr(half);
+
+  return length % 2 !== 0 || first !== second ? num * 2 : num;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,20 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  return arr.sort((a, b) => {
+    const first = a.split('').reduce((acc, val) => {
+      return acc = parseInt(acc) + parseInt(val);
+    });
+    const second = b.split('').reduce((acc, val) => {
+      return acc = parseInt(acc) + parseInt(val);
+    });
+
+    switch (true) {
+      case first > second:
+        return 1;
+      case first < second:
+        return -1;
+      default:
+        return 0;
+    }
+  });
 };
